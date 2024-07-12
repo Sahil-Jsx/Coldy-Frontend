3c88aef01a1603816a87bc530dd70c7113c13b1c
Sahil Meghnathi <121001612+Sahil-Jsx@users.noreply.github.com>
2024-07-12 18:05:58 +0530

    Update environment.jsx

--- src/env/environment.jsx
+++ src/env/environment.jsx
@@ -1,7 +1,8 @@
 export const environment = {
   // baseurl: "http://localhost:3200",
   // baseurl: "http://192.168.7.123:3200",
-   baseurl: "https://coldy-backend.onrender.com",
+   // baseurl: "https://coldy-backend.onrender.com",
+   baseurl: "https://coldy-beta.onrender.com",
   //baseurl: "https://coldy-api.yashbhagwani.me",
   
 };
